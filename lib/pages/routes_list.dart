8945d509a4922e78af88fc2caa8a6da75b6b7f88
Dylan <dmj08bot@gmail.com>
2026-06-28 12:15:17 +1000

    Added a small amount of ui to the trip list page, minutes ago box next to the times and auto update on minute refresh

--- lib/pages/routes_list.dart
+++ lib/pages/routes_list.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tripview_2/data/models/trip.dart';
 
@@ -11,6 +13,43 @@ class RouteList extends StatefulWidget {
 }
 
 class RouteListState extends State<RouteList> {
+  late final ScrollController _controller;
+  late final int _firstFutureIndex;
+  static const double _itemHeight = 60;
+  TimeOfDay now = TimeOfDay.now();
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _scheduleNextTick();
+    _firstFutureIndex = widget.tripEntries.indexWhere((trip) {
+      final parts = trip.departTime.split(':');
+      final h = int.parse(parts[0]);
+      final m = int.parse(parts[1]);
+      return h > now.hour || (h == now.hour && m >= now.minute);
+    });
+
+    _controller = ScrollController(
+      initialScrollOffset: _firstFutureIndex > 0
+          ? _firstFutureIndex * _itemHeight
+          : 0.0,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _timer.cancel();
+    super.dispose();
+  }
+  void _scheduleNextTick() {
+    _timer = Timer(Duration(seconds: 60 - DateTime.now().second), () {
+      if (!mounted) return;
+      setState(() => now = TimeOfDay.now());
+      _scheduleNextTick();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,19 +62,49 @@ class RouteListState extends State<RouteList> {
         ),
       ),
       body: ListView.builder(
+        controller: _controller,
         itemCount: widget.tripEntries.length,
+        itemExtent: _itemHeight,
         itemBuilder: (context, index) {
-          final Trip trip = widget.tripEntries[index];
-          return Row(
-            spacing: 20,
-            children: [
-              Text(trip.departTime),
-              Text(trip.arriveTime)
-            ],
+          final trip = widget.tripEntries[index];
+
+          final parts = trip.departTime.split(':');
+          final h = int.parse(parts[0]);
+          final m = int.parse(parts[1]);
+          final diff = (h*60 - now.hour*60 + m - now.minute);
+          final isPast = diff < 0;
+
+          return SizedBox(
+            height: _itemHeight,
+            child: Row(
+              children: [
+                Container(
+                  height: _itemHeight,
+                  width: _itemHeight,
+                  color: isPast ? Colors.blue.withValues(alpha: 150) : Colors.blue,
+                  padding: EdgeInsets.all(4),
+                  child: Center(child: Text(
+                    !isPast ? '${diff.abs()} mins' : '${diff.abs()} mins ago'
+                  )),
+                ),
+                Row(
+                  spacing: 20,
+                  children: [
+                    Text(
+                      trip.departTime,
+                      style: TextStyle(color: isPast ? Colors.grey : null),
+                    ),
+                    Text(
+                      trip.arriveTime,
+                      style: TextStyle(color: isPast ? Colors.grey : null),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
         },
       ),
     );
   }
-
 }
